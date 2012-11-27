de1336d3dd2fef8a43386fb1ab97370be976ce30
Anthony Tresontani <tresontani@gmail.com>
2012-11-27 08:18:51 +0100

    Add override function

--- ddi.js
+++ ddi.js
@@ -1,13 +1,14 @@
 "use strict";
 
-var snippet = {
-    text:  "<input type='text' name='{{ name }}'></input>"
-}
 
 
 
 function Composer(params) {
     var self = this;
+
+    self.snippet = {
+        text:  "<input type='text' name='{{ name }}'></input>"
+    }
 
     self.id = "composer";
     if ( typeof params.id !== "undefined" ){
@@ -19,12 +20,16 @@ function Composer(params) {
         self.fields = params.fields;
     }
 
+    self.override = function(pattern, value){
+        self.snippet[pattern] = value;
+    }
+
     self.render =  function() {
         var rendering = "";
         rendering += "<div id='" + self.id + "'>";
         
         for (var propt in self.fields){
-            rendering += Mustache.to_html(snippet[self.fields[propt].type], {name: propt});
+            rendering += Mustache.to_html(self.snippet[self.fields[propt].type], {name: propt});
         }
 
         rendering += "</div>";
--- tests.js
+++ tests.js
@@ -2,16 +2,28 @@
 
 describe("DDI", function() {
 
-  it("Should create a with the id", function() {
+  it("should create a with the id", function() {
     var composer = new Composer({id: "myComposer"});
     expect(composer.render()).toBe("<div id='myComposer'></div>");
   });
-  it("Should create an int with an input", function() {
+
+  it("should create an interface with an input", function() {
     var composer = new Composer({fields: {
             name: {type: "text"}
         }
     });
     expect(composer.render()).toBe("<div id='composer'><input type='text' name='name'></input></div>");
+  });
+
+
+  it("should create a customised input field", function() {
+    var composer = new Composer({fields: {
+            label: {type: "text"},
+        }
+    });
+
+    composer.override("text", "prop name {{ name }}");
+    expect(composer.render()).toBe("<div id='composer'>prop name label</div>");
   });
   
 });
